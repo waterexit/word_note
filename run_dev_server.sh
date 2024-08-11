9d3f24458aac82823314955d8bdc5dae34c3d8ea
water <water@dummy.com>
2024-08-11 23:32:07 +0900

    add vite server runcmd

--- run_dev_server.sh
+++ run_dev_server.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# rails server
 rm -rf /app/${PROJECT_NAME}_api/tmp/pids/server.pid
 bundle update --gemfile /app/${PROJECT_NAME}_api/Gemfile 
 cd /app/${PROJECT_NAME}_api
-rails server -b 0.0.0.0
+rails server -b 0.0.0.0 -d
 
-# cd /app/${PROJECT_NAME}_front
-# npm install
+#vite server
+cd /app/${PROJECT_NAME}_front
+npm install
+npm run dev -- --host &
+
+
