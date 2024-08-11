920e17596799a23f3d1e3aa7268343e89df80460
water <water@dummy.com>
2024-08-12 01:09:10 +0900

    add component Word

--- word_note_front/src/App.tsx
+++ word_note_front/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Word from './list/components/Word.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,8 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Word word='test' />
       <h1>Vite + React</h1>
-      <div className="card">
+        <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
---
+++ word_note_front/src/list/components/Word.tsx
@@ -0,0 +1,34 @@
+export default function Word(prop:{word:string}) {
+  return (
+    <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+      <div className="p-4 md:p-5">
+        <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+          {prop.word}
+        </h3>
+        <p className="mt-2 text-gray-500 dark:text-neutral-400">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a
+          className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 decoration-2 hover:text-blue-700 hover:underline focus:underline focus:outline-none focus:text-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-600 dark:focus:text-blue-600"
+          href="#"
+        >
+          Card link
+          <svg
+            className="shrink-0 size-4"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="m9 18 6-6-6-6"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+}
